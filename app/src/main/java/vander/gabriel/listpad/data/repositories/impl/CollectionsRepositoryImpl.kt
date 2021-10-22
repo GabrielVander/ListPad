f0bde754f6ddfcd4334ccac53ed320a97adaebac
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2021-10-22 20:28:11 -0300

    [ListPad-5] Fully implemented the CollectionsRepository

--- app/src/main/java/vander/gabriel/listpad/data/repositories/impl/CollectionsRepositoryImpl.kt
+++ app/src/main/java/vander/gabriel/listpad/data/repositories/impl/CollectionsRepositoryImpl.kt
@@ -1,19 +1,30 @@
 package vander.gabriel.listpad.data.repositories.impl
 
+import org.mapstruct.factory.Mappers
 import vander.gabriel.listpad.data.datasources.CollectionsDataSource
 import vander.gabriel.listpad.data.repositories.CollectionsRepository
+import vander.gabriel.listpad.data.repositories.mappers.CollectionMapper
 import vander.gabriel.listpad.domain.entities.CollectionEntity
+import java.util.stream.Collectors
 
 /**
  * The default CollectionsDataSource implementation
  */
-class CollectionsRepositoryImpl(private val dataSource: CollectionsDataSource) :
+class CollectionsRepositoryImpl(
+    private val dataSource: CollectionsDataSource,
+) :
     CollectionsRepository {
+
+    private val collectionMapper: CollectionMapper = Mappers.getMapper(CollectionMapper::class.java)
 
     /**
      * Retrieves all Collections available
      */
     override suspend fun getAllCollections(): List<CollectionEntity> {
-        TODO("Not yet implemented")
+        val models = dataSource.getAllCollections()
+        return models
+            .stream()
+            .map(collectionMapper::fromModelToEntity)
+            .collect(Collectors.toList())
     }
 }
