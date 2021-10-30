3acf7357f5113b0c615ec1fc9492b46090194d45
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2021-10-29 21:14:37 -0300

    [ListPad-12] Implemented the GetAllCollectionsUseCase

---
+++ app/src/test/java/vander/gabriel/listpad/domain/usecases/GetAllCollectionsUseCaseTest.kt
@@ -0,0 +1,34 @@
+package vander.gabriel.listpad.domain.usecases
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import vander.gabriel.listpad.data.repositories.CollectionsRepository
+import vander.gabriel.listpad.domain.entities.Collection
+
+internal class GetAllCollectionsUseCaseTest {
+
+    private lateinit var useCase: GetAllCollectionsUseCase
+    private lateinit var collectionsRepository: CollectionsRepository
+
+    @BeforeEach
+    fun setUp() {
+        collectionsRepository = mock()
+        useCase = GetAllCollectionsUseCase(collectionsRepository)
+    }
+
+    @Test
+    @DisplayName("Should call repository correctly")
+    fun shouldCallRepositoryCorrectly() {
+        whenever(collectionsRepository.getAllCollections()).thenAnswer { listOf<Collection>() }
+
+        runBlocking { useCase.execute(Unit) }
+
+        verify(collectionsRepository, times(1)).getAllCollections()
+    }
+}
