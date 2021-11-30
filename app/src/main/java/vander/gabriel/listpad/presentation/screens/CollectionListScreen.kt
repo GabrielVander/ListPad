656c7c4ba81845d00d116dd5120461f847f410fb
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2021-11-29 22:12:04 -0300

    [ListPad-18] Added an empty widget display to the collections list

--- app/src/main/java/vander/gabriel/listpad/presentation/screens/CollectionListScreen.kt
+++ app/src/main/java/vander/gabriel/listpad/presentation/screens/CollectionListScreen.kt
@@ -1,6 +1,7 @@
 package vander.gabriel.listpad.presentation.screens
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,12 +13,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.InternalCoroutinesApi
+import vander.gabriel.listpad.R
+import vander.gabriel.listpad.domain.entities.Collection
 import vander.gabriel.listpad.domain.entities.NavigationRoutes
 import vander.gabriel.listpad.presentation.components.Loader
 import vander.gabriel.listpad.presentation.components.Pill
@@ -66,77 +71,115 @@ fun CollectionListScreen(
         if (loading) {
             Loader()
         } else {
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-            ) {
-                collections.forEach {
-                        (
-                            _,
-                            name,
-                            description,
-                            isUrgent,
-                            category,
-                        ),
-                    ->
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        shape = RectangleShape,
-                        elevation = COLLECTION_ELEVATION,
-                        onClick = {
-                            /* TODO */
-                        }
-                    ) {
-                        Column(
-                            modifier = Modifier
-                                .padding(all = LARGE_PADDING)
-                                .fillMaxWidth(),
-                        ) {
-                            Row {
-                                Text(
-                                    modifier = Modifier.weight(8f),
-                                    text = name,
-                                    style = MaterialTheme.typography.h5,
-                                    fontWeight = FontWeight.Bold,
-                                    maxLines = 1
-                                )
-                                if (isUrgent) {
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .weight(1f),
-                                        contentAlignment = Alignment.TopEnd
-                                    ) {
-                                        Canvas(
-                                            modifier = Modifier
-                                                .size(CATEGORY_INDICATOR_SIZE)
-                                        ) {
-                                            drawCircle(
-                                                color = Color.Red
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = description,
-                                style = MaterialTheme.typography.subtitle1,
-                                maxLines = 2,
-                                overflow = TextOverflow.Ellipsis
-                            )
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Pill(color = category.color, label = category.name)
-                        }
-                    }
-                }
+            if (collections.isEmpty()) {
+                EmptyContent()
+            } else {
+                ListContent(collections)
             }
         }
 
     }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun ListContent(collections: List<Collection>) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        collections.forEach {
+                (
+                    _,
+                    name,
+                    description,
+                    isUrgent,
+                    category,
+                ),
+            ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = COLLECTION_ELEVATION,
+                onClick = {
+                    /* TODO */
+                }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(all = LARGE_PADDING)
+                        .fillMaxWidth(),
+                ) {
+                    Row {
+                        Text(
+                            modifier = Modifier.weight(8f),
+                            text = name,
+                            style = MaterialTheme.typography.h5,
+                            fontWeight = FontWeight.Bold,
+                            maxLines = 1
+                        )
+                        if (isUrgent) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f),
+                                contentAlignment = Alignment.TopEnd
+                            ) {
+                                Canvas(
+                                    modifier = Modifier
+                                        .size(CATEGORY_INDICATOR_SIZE)
+                                ) {
+                                    drawCircle(
+                                        color = Color.Red
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = description,
+                        style = MaterialTheme.typography.subtitle1,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Pill(color = category.color, label = category.name)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EmptyContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            painter = painterResource(id = R.drawable.ic_dissatisfied_24),
+            contentDescription = "Sad face icon",
+        )
+        Text(
+            text = "No collections!",
+            fontWeight = FontWeight.Bold,
+            fontSize = MaterialTheme.typography.h6.fontSize
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun EmptyContentPreview() {
+    EmptyContent()
 }
